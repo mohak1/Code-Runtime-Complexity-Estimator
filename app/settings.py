f69d3a160f71abd2e5bee042da184c8480e65284
mohak <narangmohak9@gmail.com>
2022-11-20 18:49:52 +0000

    updating COMPILER_BASE_URL

--- app/settings.py
+++ app/settings.py
@@ -1,14 +1,15 @@
+# standard library imports
 import os
 
 MAX_INPUTS = 100    # num inputs to generate
 FLOAT_ALLOWED_CODES_LIST = ['01', '11']   # is float allowed
 FLOAT_ALLOWED_CODE = '01'
 INT_ALLOWED_CODE = '10'
+STRING_INPUT_CODE = '0'
+COMPILER_BASE_URL = 'http://localhost:2358'
 
 inside_docker = os.getenv('is_dockerised', False)
 if inside_docker:
-    COMPILER_BASE_URL = 'http://judge0-server-1:2358'
     debug_state = False # no debug for deployed app
 else:
-    COMPILER_BASE_URL = 'http://localhost:2358'
     debug_state = True
