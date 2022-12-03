ea9a099e4a2dd58f9f97b9e29e2fe7039cb8fefa
mohak <narangmohak9@gmail.com>
2022-12-03 17:29:12 +0000

    updating method for sending discord messages

--- app/helpers/notifs.py
+++ app/helpers/notifs.py
@@ -1,5 +1,14 @@
 """sends a notification to discord"""
+import os
 
-def send_message_on_discord(message):
-    # TODO: integrate discord!!!!
-    print(message)
+import requests
+
+def send_message_on_discord(message: str) -> None:
+    if (discord_server_webhook_url := os.getenv('DISCORD_URL', None)):
+        requests.post(
+            url=discord_server_webhook_url,
+            json={"content": message},
+            headers={"Content-Type": "application/json"}
+        )
+    else:
+        print(message)
