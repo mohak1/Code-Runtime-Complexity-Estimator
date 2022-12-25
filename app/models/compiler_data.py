bd99ed2bdf993973a4af8354d6fe5808a9bf4578
mohak <narangmohak9@gmail.com>
2022-12-25 22:32:06 +0000

    removing unused method

--- app/models/compiler_data.py
+++ app/models/compiler_data.py
@@ -5,23 +5,10 @@ from typing import List
 # external imports
 from pydantic import BaseModel
 
-# internal imports
-from helpers import custom_exceptions
-
 class ActiveLanguagesListItem(BaseModel):
     id: str
     name: str
 
-
-# might not need the ActiveLanguageList model (and the parse fn)
-# might just return the json that is returned by judge0
 class ActiveLanguagesList(BaseModel):
     """response structure of judge0 languages endpoint"""
     __root__: List[ActiveLanguagesListItem]
-
-
-def parse_to_active_language_model(data: List[dict]) -> ActiveLanguagesList:
-    try:
-        return ActiveLanguagesList.parse_obj(data).__root__
-    except Exception as e:
-        raise custom_exceptions.ParsingToPydanticModelException(data, e)
